a889089752a2ddf9ad925d92317f72b317765c1e
dragon310798 <nguyehoa020@mysbisd.org>
2017-01-12 13:26:17 -0600

    deleted the pyc file

--- team0.py
+++ team0.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'DEMACIA-0' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'De0'
+strategy_description = 'betray the first round. then betray unless last round, all players colluded'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history) == 0:
+        return 'b'
+    elif my_history[-1] == 'c' and \
+            their_history[-1] == 'c':
+        return 'c'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
